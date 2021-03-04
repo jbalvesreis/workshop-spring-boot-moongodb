f085ec6108fbbf42c26bce6fe39e9565e99cef2f
jbalvesreis <jbalves1961@gmail.com>
2021-03-04 17:18:13 -0300

    atualizacao de Usuario com PUT

--- src/main/java/com/example/worskopmongo/resources/UserResource.java
+++ src/main/java/com/example/worskopmongo/resources/UserResource.java
@@ -50,4 +50,13 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method=RequestMethod.PUT)
+ 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.insert(obj);		
+		return ResponseEntity.noContent().build();
+	}	
+
 }
