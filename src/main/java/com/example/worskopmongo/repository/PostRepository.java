30e9dbb22d3eabb677551eee4c480384528b5575
jbalvesreis <jbalves1961@gmail.com>
2021-03-08 11:05:54 -0300

    Crianto entity Post com Use aninhado

---
+++ src/main/java/com/example/worskopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.example.worskopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.worskopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
