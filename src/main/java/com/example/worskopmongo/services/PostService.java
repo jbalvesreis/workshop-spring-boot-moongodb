ce49ae43029430b23e85a00e22b5295eafd6f05c
jbalvesreis <jbalves1961@gmail.com>
2021-03-09 10:19:24 -0300

    Consulta simples com query methods

--- src/main/java/com/example/worskopmongo/services/PostService.java
+++ src/main/java/com/example/worskopmongo/services/PostService.java
@@ -6,9 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.worskopmongo.domain.User;
-import com.example.worskopmongo.dto.UserDTO;
-import com.example.worskopmongo.repository.UserRepository;
+import com.example.worskopmongo.domain.Post;
+import com.example.worskopmongo.repository.PostRepository;
 import com.example.worskopmongo.services.exception.ObjectNotFoundException;
 
 
@@ -16,37 +15,15 @@ import com.example.worskopmongo.services.exception.ObjectNotFoundException;
 public class PostService {
 	
 	@Autowired
-	private UserRepository repo;
+	private PostRepository repo;
 	
-	public List<User> findAll() {
-		return repo.findAll();
-	}
-	
-	public User findById(String id) {
-		Optional<User> obj = repo.findById(id);
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
-	public User insert(User obj) {
-		return repo.insert(obj);		
-	}
-	
-	public void delete(String id) {
-		findById(id);
-		repo.deleteById(id);
-	}
-	
-	public User update(User entity) {
-		User newObj = repo.findById(entity.getId()).get();
-		updateData(newObj, entity);
-		return repo.save(newObj);	}
-	
-	private void updateData(User newObj, User obj) {
-		newObj.setName(obj.getName());
-		newObj.setEmail(obj.getEmail());		
-	}
 
-	public User fromDTO(UserDTO objDto) {
-		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());		
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
+	
 }
